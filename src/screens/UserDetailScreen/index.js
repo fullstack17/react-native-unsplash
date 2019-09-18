b5c1c0d19da7fa0d5d5bb32acc05f3b16af38ceb
Henryk Apostel <henrykapostel113@gmail.com>
2019-09-18 22:26:26 +0400

    implemented User Detail screen

--- src/screens/UserDetailScreen/index.js
+++ src/screens/UserDetailScreen/index.js
@@ -1,22 +1,142 @@
 import React, { Component } from 'react';
 import { SafeAreaView } from 'react-navigation';
-import { View } from 'react-native';
+import _ from 'lodash';
+import { View, Image, Text, FlatList } from 'react-native';
+import { connect } from 'react-redux';
 import { styles } from './styles';
-
+import { CommonStyle } from '../../themes';
+import { Header } from '../../components';
+import { callAPI } from '../../service/api';
+import { selectedUserSelector } from '../../redux/selector';
+import { SelectedUserActions } from '../../redux';
 class UserDetailScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      photos: []
+    }
+    this.nextPagination = 1;
+    this.pageSize = 10;
+  }
+
+  componentDidMount() {
+    this.onGetUserImages();
+  }
+
+  onGetUserImages = async () => {
+    const { navigation } = this.props;
+    const photoLink = _.get(navigation, 'state.params.userDetail.links.photos', null);
+    if (photoLink) {
+      this.setState({ isFetching: true });
+      const res = await callAPI(photoLink, this.nextPagination, this.pageSize);
+      this.setState({ isFetching: false });
+      if (res.error) {
+        alert('error');
+      } else {
+        if (res.length < this.pageSize) {
+          this.nextPagination = -1; // no more
+        } else {
+          this.nextPagination += 1;
+        }
+        this.props.addImages(res);
+      }
     }
   }
 
+  onLoadMore = () => {
+    const { isFetching } = this.state;
+    if (this.nextPagination != -1 && !isFetching) {
+      this.onGetUserImages();
+    }
+  }
+
+  onBack = () => {
+    this.props.clearImages();
+    this.props.navigation.goBack();
+  }
+
+  renderUserInfo = () => {
+    const { navigation } = this.props;
+    const name = _.get(navigation, 'state.params.userDetail.name', '');
+    const userName = _.get(navigation, 'state.params.userDetail.username', '');
+    const userPhoto = _.get(navigation, 'state.params.userDetail.profile_image.large', null);
+
+    return (
+      <View style={styles.userInfoContainer}>
+        {userPhoto && <Image
+          source={{ uri: userPhoto }}
+          style={styles.userImage}
+        />}
+        {!userPhoto && <Image source={require('../../assets/img_placeholder.jpg')} style={styles.userImage}/>}
+        <View style={styles.userSubInfoContainer}>
+          <Text style={styles.whiteBoldText}>
+            {name}
+          </Text>
+          <Text style={styles.whiteText}>
+            {userName}
+          </Text>
+        </View>
+      </View>
+    );
+  }
+
+  renderSeparator = () => <View style={styles.separator} />;
+
+  renderUserImage = ({ item, index }) => {
+    const uri = _.get(item, 'urls.small', null);
+    if (!uri) {
+      return <Image source={require('../../assets/img_placeholder.jpg')} style={styles.userImageContainer}/>;
+    }
+    return (
+      <Image
+        style={styles.userImageContainer}
+        source={{ uri }}
+      />
+    );
+  }
+
+  renderUserImages = () => {
+    const { photos } = this.state;
+    const { navigation, selectedUser } = this.props;
+    const images = _.get(navigation, 'state.params.userDetail')
+    return (
+      <View style={styles.userImagesContainer}>
+        <Text style={styles.whiteBigBoldText}>
+          User Images
+        </Text>
+        <FlatList
+          data={selectedUser.images}
+          numColumns={2}
+          renderItem={this.renderUserImage}
+          keyExtractor={(item, index) => `index-${index}`}
+          ItemSeparatorComponent={this.renderSeparator}
+          style={styles.imagesList}
+          onEndReached={this.onLoadMore}
+        />
+      </View>
+    );
+  };
+
   render() {
     return (
-      <SafeAreaView style={styles.container}>
-
+      <SafeAreaView style={CommonStyle.container}>
+        <Header title="User Details" onBack={this.onBack} />
+        <View style={styles.mainContainer}>
+          {this.renderUserInfo()}
+          {this.renderUserImages()}
+        </View>
       </SafeAreaView>
     );
   }
 }
 
-export default UserDetailScreen;
+const mapStateToProps = state => ({
+  ...selectedUserSelector(state)
+});
+
+const mapDispatchToProps = dispatch => ({
+  addImages: images => dispatch(SelectedUserActions.addUserImages(images)),
+  clearImages: () => dispatch(SelectedUserActions.clearUserImages())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserDetailScreen);
--- src/screens/UserDetailScreen/styles.js
+++ src/screens/UserDetailScreen/styles.js
@@ -1,8 +1,57 @@
 import { StyleSheet } from 'react-native';
+import { Color, Font, Metric } from '../../themes';
 
 export const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
     flex: 1,
-    backgroundColor: 'steelblue'
+    marginHorizontal: 5,
+    marginBottom: 10
+  },
+  userImage: {
+    width: 120,
+    height: 120,
+    resizeMode: 'contain',
+    borderRadius: 5,
+    marginLeft: 5
+  },
+  userInfoContainer: {
+    flexDirection: 'row'
+  },
+  userSubInfoContainer: {
+    marginLeft: 20,
+    justifyContent: 'center'
+  },
+  whiteBoldText: {
+    color: Color.white,
+    fontSize: Font.size.regular,
+    fontWeight: Font.weight.bold
+  },
+  whiteBigBoldText: {
+    color: Color.white,
+    fontSize: Font.size.large,
+    fontWeight: Font.weight.bold,
+    marginLeft: 5
+  },
+  whiteText: {
+    color: Color.white,
+    fontSize: Font.size.medium,
+  },
+  separator: {
+    height: 5,
+  },
+  userImageContainer: {
+    width: (Metric.width - 30) / 2,
+    height: (Metric.width - 30) / 2,
+    marginHorizontal: 5,
+    resizeMode: 'cover',
+    borderRadius: 5
+  },
+  userImagesContainer: {
+    marginTop: 20,
+    flex: 1
+  },
+  imagesList: {
+    marginTop: 10,
+    flex: 1
   }
 });
