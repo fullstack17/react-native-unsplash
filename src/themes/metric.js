d608acadcb9af48eeff360c31c7bde1e32707c9d
Henryk Apostel <henrykapostel113@gmail.com>
2019-09-19 00:44:56 +0400

    updated metric themes

--- src/themes/metric.js
+++ src/themes/metric.js
@@ -2,7 +2,24 @@ import { Dimensions } from 'react-native';
 
 const { width, height } = Dimensions.get('window');
 
+const wp = (percentage) => {
+  const value = (percentage * width) / 100;
+  return Math.round(value);
+}
+
+const carouselSlideHeight = (height / 3) * 2;
+const carouselSlideWidth = wp(75);
+const carouselItemHorizontalMargin = wp(2);
+
+export const carouselSliderWidth = width;
+export const carouselItemWidth = carouselSlideWidth + carouselItemHorizontalMargin * 2;
+
 export default {
   width,
-  height
+  height,
+  carouselItemHorizontalMargin,
+  carouselSlideWidth,
+  carouselSlideHeight,
+  carouselSliderWidth,
+  carouselItemWidth
 }
