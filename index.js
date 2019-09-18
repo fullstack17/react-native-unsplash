219000e30112baf679155e38407e8b51276584a7
Henryk Apostel <henrykapostel113@gmail.com>
2019-09-18 19:19:37 +0400

    updated status bar

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native';
+import { AppRegistry, StatusBar } from 'react-native';
 import App from './src/app';
 import { name as appName } from './app.json';
 
+StatusBar.setBarStyle('light-content', false);
 AppRegistry.registerComponent(appName, () => App);
