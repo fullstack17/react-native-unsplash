d9516f84bbc67ae3ef54c776bd7821c8d310292a
Henryk Apostel <henrykapostel113@gmail.com>
2019-09-18 22:25:10 +0400

    added redux for selected user images

--- src/redux/selector/index.js
+++ src/redux/selector/index.js
@@ -4,3 +4,8 @@ const user$ = state => state.user;
 export const userSelector = createSelector(user$, user => ({
   user
 }));
+
+const selectedUser$ = state => state.selectedUser;
+export const selectedUserSelector = createSelector(selectedUser$, selectedUser => ({
+  selectedUser
+}));
