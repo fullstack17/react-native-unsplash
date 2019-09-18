2a2fa3b6ae74c1410b612887c31ef124f2b04ca6
Henryk Apostel <henrykapostel113@gmail.com>
2019-09-18 22:26:40 +0400

    added api to get the images of user

--- src/service/api.js
+++ src/service/api.js
@@ -7,6 +7,9 @@ const { Unsplash } = AppConfig;
 export const getUnsplashUsers = async (page, query) => {
   const res = await apiModels(`search/users?per_page=15&page=${page}&query=${query}&client_id=${Unsplash.clientID}`, 'GET', null, null);
   console.info('res getUnsplashUsers', res);
-  debugger;
   return res;
 };
+
+export const callAPI = async (link, page, pageSize = 25) => {
+  return apiModels(`${link}?per_page=${pageSize}&page=${page}&client_id=${Unsplash.clientID}`, 'GET', null, null, true);
+}
